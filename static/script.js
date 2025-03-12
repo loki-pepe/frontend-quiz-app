671c1048a36a7942ae0ab03bf4cbec1143658878
Lovro <lovroperaic@gmail.com>
2025-03-12 17:15:14 +0100

    Inject subject buttons in home page

--- static/script.js
+++ static/script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const SUBJECTS = document.querySelector('#subjects');
+
+    fetch('data.json').then(response => {
+        return response.json();
+    }).then(data => {
+        for (let quiz of data.quizzes) {
+            let subjectContainer = document.createElement('li');
+            let subject = document.createElement('button');
+            let subjectImage = document.createElement('img');
+
+            subjectImage.src = quiz.icon;
+            
+            subject.textContent = quiz.title;
+            subject.prepend(subjectImage);
+            subject.id = quiz.title;
+
+            subjectContainer.appendChild(subject);
+            SUBJECTS.appendChild(subjectContainer);
+        }
+    });
 });
